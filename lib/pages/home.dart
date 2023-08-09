d30fdd37ad356f513008e5508e735c1633d7b46b
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-08-09 09:46:02 -0300

    barra de pesquisa

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,48 +7,84 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Breakfast",
-          style: TextStyle(
-              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
-        ),
+        appBar: appBar(),
         backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: GestureDetector(
+        body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(0.11),
+                    blurRadius: 20,
+                    spreadRadius: 0.0)
+              ]),
+              child: TextField(
+                decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(15),
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Search Pancake',
+                    hintStyle:
+                        TextStyle(color: Color.fromARGB(255, 216, 216, 216)),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: SvgPicture.asset('assets/icons/Search.svg'),
+                    ),
+                    suffixIcon: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: SvgPicture.asset('assets/icons/Filter.svg'),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none)),
+              ),
+            )
+          ],
+        ));
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        "Breakfast",
+        style: TextStyle(
+            color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
+          ),
+          decoration: BoxDecoration(
+              color: Colors.grey[50], borderRadius: BorderRadius.circular(10)),
+        ),
+      ),
+      actions: [
+        GestureDetector(
           onTap: () {},
           child: Container(
             margin: const EdgeInsets.all(10),
             alignment: Alignment.center,
             child: SvgPicture.asset(
-              'assets/icons/Arrow - Left 2.svg',
-              height: 20,
-              width: 20,
+              'assets/icons/dots.svg',
+              height: 5,
+              width: 5,
             ),
             decoration: BoxDecoration(
                 color: Colors.grey[50],
                 borderRadius: BorderRadius.circular(10)),
           ),
-        ),
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              child: SvgPicture.asset(
-                'assets/icons/dots.svg',
-                height: 5,
-                width: 5,
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.grey[50],
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
