f4d1e7f1a343098fd5fab3ec51d420e6f1d7090d
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-08-09 12:16:20 -0300

    acertando bugs

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -120,6 +120,7 @@ class _HomePageState extends State<HomePage> {
 
   Column _dietsSection() {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Padding(
           padding: EdgeInsets.only(left: 20),
@@ -129,20 +130,24 @@ class _HomePageState extends State<HomePage> {
                 color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
           ),
         ),
-        const SizedBox(height: 15),
-        SizedBox(
-            height: 240,
-            child: ListView.separated(
-              itemBuilder: (context, index) {
-                return Container(
-                    width: 210,
-                    decoration: BoxDecoration(
-                        color: diets[index].boxColor.withOpacity(0.5),
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        const SizedBox(
+          height: 15,
+        ),
+        Container(
+          height: 240,
+          child: ListView.separated(
+            itemBuilder: (context, index) {
+              return Container(
+                width: 210,
+                decoration: BoxDecoration(
+                    color: diets[index].boxColor.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SvgPicture.asset(diets[index].iconPath),
+                    Column(
                       children: [
-                        SvgPicture.asset(diets[index].iconPath),
                         Text(
                           diets[index].name,
                           style: const TextStyle(
@@ -152,45 +157,54 @@ class _HomePageState extends State<HomePage> {
                         ),
                         Text(
                           diets[index].level +
-                              " | " +
+                              ' | ' +
                               diets[index].duration +
-                              " | " +
+                              ' | ' +
                               diets[index].calorie,
-                          style: TextStyle(
-                              color: Colors.blueGrey,
+                          style: const TextStyle(
+                              color: Color(0xff7B6F72),
                               fontSize: 13,
                               fontWeight: FontWeight.w400),
                         ),
-                        Container(
-                          height: 45,
-                          width: 130,
-                          child: Center(
-                              child: Text("View",
-                                  style: TextStyle(
-                                      color: diets[index].viewIsSelected
-                                          ? Colors.white
-                                          : Color.fromARGB(255, 255, 205, 222),
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 14))),
-                          decoration: BoxDecoration(
-                              gradient: LinearGradient(colors: [
-                                diets[index].viewIsSelected
-                                    ? const Color(0xFF9CCDF8)
-                                    : Colors.transparent,
-                                diets[index].viewIsSelected
-                                    ? Color.fromARGB(255, 205, 233, 255)
-                                    : Colors.transparent
-                              ]),
-                              borderRadius: BorderRadius.circular(50)),
-                        )
                       ],
-                    ));
-              },
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              separatorBuilder: (context, index) => const SizedBox(width: 25),
-              itemCount: diets.length,
-            ))
+                    ),
+                    Container(
+                      height: 45,
+                      width: 130,
+                      child: Center(
+                        child: Text(
+                          'View',
+                          style: TextStyle(
+                              color: diets[index].viewIsSelected
+                                  ? Colors.white
+                                  : const Color(0xffC58BF2),
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [
+                            diets[index].viewIsSelected
+                                ? const Color(0xff9DCEFF)
+                                : Colors.transparent,
+                            diets[index].viewIsSelected
+                                ? const Color(0xff92A3FD)
+                                : Colors.transparent
+                          ]),
+                          borderRadius: BorderRadius.circular(50)),
+                    )
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const SizedBox(
+              width: 25,
+            ),
+            itemCount: diets.length,
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20, right: 20),
+          ),
+        )
       ],
     );
   }
